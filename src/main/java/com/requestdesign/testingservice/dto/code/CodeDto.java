97278b3ffc4d9eba5d1b1ec85a89697f8bea65ed
fedor <fedor.markov.1@mail.ru>
2022-06-15 02:33:04 +0500

    feat: Создан CodeDto

---
+++ src/main/java/com/requestdesign/testingservice/dto/code/CodeDto.java
@@ -0,0 +1,13 @@
+package com.requestdesign.testingservice.dto.code;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+public class CodeDto {
+    private Long resultId;
+    private Long respondentId;
+}
