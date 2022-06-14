c9b718a7492ba4f9888856b68ddf766db3a90d8a
fedor <fedor.markov.1@mail.ru>
2022-06-15 03:24:58 +0500

    feat: Добавлен контроллер result

--- src/main/java/com/requestdesign/testingservice/controller/result/ResultController.java
+++ src/main/java/com/requestdesign/testingservice/controller/result/ResultController.java
@@ -1,11 +1,33 @@
 package com.requestdesign.testingservice.controller.result;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/result")
 @Slf4j
-public class ResultController {
+public class ResultController implements ResultControllerInterface{
+    private final ResultService resultService;
+
+    @Autowired
+    public ResultController(ResultService resultService) {
+        this.resultService = resultService;
+    }
+    @Override
+    public ResponseEntity getResultById(Integer resultId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity getTaskFromResultById(Integer resultId, Integer taskId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity getQuestionAnswerFromResultById(Integer resultId, Integer questionId) {
+        return null;
+    }
 }
