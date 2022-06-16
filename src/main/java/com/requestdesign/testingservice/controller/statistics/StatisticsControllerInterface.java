b92cae73acd6bb5a5c0f0d311b5ecaaacecaca83
fedor <fedor.markov.1@mail.ru>
2022-06-16 19:28:44 +0500

    feat: Изменен роут для получения статистики

--- src/main/java/com/requestdesign/testingservice/controller/statistics/StatisticsControllerInterface.java
+++ src/main/java/com/requestdesign/testingservice/controller/statistics/StatisticsControllerInterface.java
@@ -4,6 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public interface StatisticsControllerInterface {
-    @GetMapping("/statistic")
+    @GetMapping("/")
     public ResponseEntity getAllStatistic();
 }
